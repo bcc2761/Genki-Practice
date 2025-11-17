786b9a32165178d679edec44d7a2c45d6748fae5
Bennett <bcc2761@g.rit.edu>
2025-11-17 12:02:28 -0500

    models.py foundation

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -2,7 +2,7 @@
 # Using SQLAlchemy with SQLite for simplicity
 # Allows easy database interactions using Python classes to represent tables
 
-from sqlalchemy import Column, Integer, String, DataTime, create_engine # Core SQLAlchemy components
+from sqlalchemy import Column, Integer, String, DateTime, create_engine # Core SQLAlchemy components
 from sqlalchemy.ext.declarative import declarative_base # Base class for ORM models
 from sqlalchemy.orm import sessionmaker # Session factory for database interactions
 from datetime import datetime # For timestamp fields
@@ -17,6 +17,36 @@ class Challenge(Base):
 
     # Challenge model columns
     id = Column(Integer, primary_key=True, index=True) # Primary key column
-    user_id = Column(String, index=True) # User identifier
-    challenge_type = Column(String, index=True) # Type of challenge (e.g., vocabulary, grammar)
-    content = Column(String) # Content of the challenge
+    chapter = Column(Integer, index=True) # Chapter number
+    date_created = Column(DateTime, default=datetime.now) # Timestamp of creation
+    created_by = Column(String, nullable=False) # Creator identifier
+    title = Column(String, nullable=False) # Title of the challenge
+    options = Column(String, nullable=False) # Possible options for the challenge
+    correct_answer_id = Column(Integer, nullable=False) # ID of the correct answer
+    explanation = Column(String, nullable=False) # Explanation for the answer
+    # user_id = Column(String, index=True) # User identifier
+    # challenge_type = Column(String, index=True) # Type of challenge (e.g., vocabulary, grammar)
+    # content = Column(String) # Content of the challenge
+
+class ChallengeQuota(Base):
+    __tablename__ = 'challenge_quotas' # Table name in the database
+
+    # ChallengeQuota model columns
+    id = Column(Integer, primary_key=True, index=True) # Primary key column
+    user_id = Column(String, nullable=False, unique=True) # User identifier
+    quota_remaining = Column(Integer, nullable=False, default=20)
+    # chapter = Column(Integer, index=True) # Chapter number
+    # quota = Column(Integer, default=0) # Number of challenges created by the user for the chapter
+    last_reset_date = Column(DateTime, default=datetime.now) # Timestamp of last quota reset
+
+Base.metadata.create_all(bind=engine) # Create tables in the database
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # Session factory
+
+def get_db():
+    # From documentation
+    db = SessionLocal()
+    try:
+        yield db # yield is a generator, allows function to return a value and later resume
+    finally:
+        db.close()
